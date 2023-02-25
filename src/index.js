18167a1d30b822711803975d2d307cd0748d52e7
saeideh-mahmoudian <s.mahmoudian@gmail.com>
2023-02-25 22:47:16 +0330

    update weather app for week 6

--- src/index.js
+++ src/index.js
@@ -1,29 +1,51 @@
-
+function insertCurrentDay(date){
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wendesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  
+  return days[date.getDay()];
+}
+  
+function insertCurrentTime(date){
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+  
 function showInformation(response){
-      let city = response.data.name;
-      let temperature = Math.round(response.data.main.temp);
-      let minTemperature = Math.round(response.data.main.temp_min);
-      let maxTemperature = Math.round(response.data.main.temp_max);
-      let weatherDescription = response.data.weather[0].description;
-      let humidity = Math.round(response.data.main.humidity);
-      let windSpeed = Math.round((response.data.wind.speed)*3.6);
-      //let precipitation = Math.round(response.data.precipitation.value);
-      let citySearch = document.querySelector("#current-city-search");
-      citySearch.innerHTML = city;
-      let temperatureValue = document.querySelector("#temperature");
-      temperatureValue.innerHTML = temperature;
-      let minTemp = document.querySelector("#minTemp");
-      minTemp.innerHTML = `${minTemperature}°C`;
-      let maxTemp = document.querySelector("#maxTemp");
-      maxTemp.innerHTML = `${maxTemperature}°C`;
-      let description = document.querySelector("#description");
-      description.innerHTML = weatherDescription;
-      let humidityLevel = document.querySelector("#humidity");
-      humidityLevel.innerHTML = `${humidity}%`;
-      let windLevel = document.querySelector("#wind");
-      windLevel.innerHTML = `${windSpeed}km/h`;
-      //let precipitationLevel = document.querySelector("#precipitation");
-      //precipitationLevel.innerHTML = `${precipitation}mm`;
+  let city = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let minTemperature = Math.round(response.data.main.temp_min);
+  let maxTemperature = Math.round(response.data.main.temp_max);
+  let weatherDescription = response.data.weather[0].description;
+  let humidity = Math.round(response.data.main.humidity);
+  let windSpeed = Math.round((response.data.wind.speed)*3.6);
+  let citySearch = document.querySelector("#current-city-search");
+  citySearch.innerHTML = city;
+  let temperatureValue = document.querySelector("#temperature");
+  temperatureValue.innerHTML = temperature;
+  let minTemp = document.querySelector("#minTemp");
+  minTemp.innerHTML = `${minTemperature}°C`;
+  let maxTemp = document.querySelector("#maxTemp");
+  maxTemp.innerHTML = `${maxTemperature}°C`;
+  let description = document.querySelector("#description");
+  description.innerHTML = weatherDescription;
+  let humidityLevel = document.querySelector("#humidity");
+  humidityLevel.innerHTML = `${humidity}%`;
+  let windLevel = document.querySelector("#wind");
+  windLevel.innerHTML = `${windSpeed}km/h`;
 }
 
 let searchForm = document.querySelector("#search-form");
@@ -42,7 +64,7 @@ searchForm.addEventListener("submit", function (event) {
   }
 });
 
-let currentButton = document.querySelector("#button");
+let currentButton = document.querySelector("#current-location-button");
 currentButton.addEventListener("click", function(){
   navigator.geolocation.getCurrentPosition(function(position){
   let lat = position.coords.latitude;
@@ -54,29 +76,13 @@ currentButton.addEventListener("click", function(){
   });
 });
 
-/////Time settings/////
-let nowDate = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wendesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let day = days[nowDate.getDay()];
 let day_value = document.querySelector("#currentDay");
-day_value.innerHTML = day;
+let nowDate = new Date();
+day_value.innerHTML = insertCurrentDay(nowDate);
 
-let hours = nowDate.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = nowDate.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
 let time_value = document.querySelector("#currentTime");
-time_value.innerHTML = `${hours}:${minutes}`;
+time_value.innerHTML = insertCurrentTime(nowDate);
+
+let apiKey = "cf341738c7bb0fba2e56905d21ee16f0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&units=metric&appid=${apiKey}`;
+axios.get(apiUrl).then(showInformation);
